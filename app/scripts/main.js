1c4935fb4e8c2a65b0a503a24d280ae9fa5cee12
Matt Wildman <matthewwildman@gmail.com>
2016-06-13 15:38:43 +0100

    Moved CV data into .json file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,98 +1,28 @@
 (function() {
 	var app = angular.module('cv-tool', ['ngSanitize']);
-	app.controller('ExplorerController', function() {
-		this.cvs = [
-			{
-				id: 'beginner',
-				title: 'First CV',
-				pages: [
-					{
-						name: 'Page one',
-						sections: [
-							{
-								title: 'Heading',
-								content: '<p>Beginners\' CV heading section</p>'
-							},
-							{
-								title: 'Statement',
-								content: '<p>Beginners\' CV statement</p>'
-							},
-							{
-								title: 'Education',
-								content: '<p>Beginners\' CV education</p>'
-							}
-						]
-					},
-					{
-						name: 'Page two',
-						sections: [
-							{
-								title: 'Work experience',
-								content: '<p>Beginners\' CV work experience</p>'
-							},
-							{
-								title: 'Skills',
-								content: '<p>Beginners\' CV skills</p>'
-							},
-							{
-								title: 'Interests',
-								content: '<p>Beginners\' CV interests</p>'
-							}
-						]
-					}
-				]
-			},
-			{
-				id: 'experienced',
-				title: 'Experienced hire CV',
-				pages: [
-					{
-						name: 'Relevant experience',
-						sections: [
-							{
-								title: 'Heading',
-								content: '<p>Experienced CV heading section</p>'
-							},
-							{
-								title: 'Statement',
-								content: '<p>Experienced CV statement</p>'
-							},
-							{
-								title: 'Education',
-								content: '<p>Experienced CV education</p>'
-							}
-						]
-					},
-					{
-						name: 'Full history',
-						sections: [
-							{
-								title: 'Work experience',
-								content: '<p>Experienced CV work experience</p>'
-							},
-							{
-								title: 'Skills',
-								content: '<p>Experienced CV skills</p>'
-							},
-							{
-								title: 'Interests',
-								content: '<p>Experienced CV interests</p>'
-							}
-						]
-					}
-				]
-			}
-		];
-		this.currentCV = this.cvs[0];
+	app.controller('ExplorerController', ['$http', function($http) {
+		
+		var explorer = this;
+
+		this.cvs = [];
+		this.currentIndex = 0;
+		this.currentCV = {};
 		this.currentPage = 0;
 		this.setCV = function(index) {
-			this.currentCV = this.cvs[index];
+			this.currentIndex = index;
+			this.currentCV = this.cvs[this.currentIndex];
 			this.setPage(0);
 		};
 		this.setPage = function(index) {
 			this.currentPage = index;
 		};
-	});
+
+		$http.get('cvs.json').success(function(data) {
+			explorer.cvs = data;
+			explorer.setCV(0);
+		});
+
+	}]);
 	app.directive('cv', function() {
 		return {
 			restrict: 'E',
